586bcefcdcf723579f6c0ae98938963881827504
Moncef Belyamani <giter@done>
2013-03-29 16:55:39 -0700

    updated README

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,11 @@ class HomeController < ApplicationController
 		from = Date.today
   	@current_year = from.year
   	to = Date.civil(@current_year,12,31)
-  	upcoming_holidays = Holidays.between(from, to, :us)
+
+  	@country = request.location.country
+  	@country_code = request.location.country_code.lowercase
+  	
+  	upcoming_holidays = Holidays.between(from, to, @country_code.to_symbol)
   	@next_holiday_name = upcoming_holidays[0][:name]
   	@next_holiday_date = upcoming_holidays[0][:date]
   	@next_holiday_month_day = @next_holiday_date.day

b987c6e4410f16175d7c03930fd6085c9909b23a
Moncef Belyamani <giter@done>
2013-03-29 17:09:27 -0700

    use unicorn on heroku via procfile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,9 @@ class HomeController < ApplicationController
   	to = Date.civil(@current_year,12,31)
 
   	@country = request.location.country
-  	@country_code = request.location.country_code.lowercase
+  	@country_code = request.location.country_code.downcase
   	
-  	upcoming_holidays = Holidays.between(from, to, @country_code.to_symbol)
+  	upcoming_holidays = Holidays.between(from, to, @country_code.to_sym)
   	@next_holiday_name = upcoming_holidays[0][:name]
   	@next_holiday_date = upcoming_holidays[0][:date]
   	@next_holiday_month_day = @next_holiday_date.day

4160e8edd318a6eac0f11a52b33a980e8651474b
Moncef Belyamani <giter@done>
2013-03-29 22:13:59 -0700

    adds error handling for unknown regions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from Holidays::UnknownRegionError do
+    @upcoming_holidays = nil
+    render :index
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,16 @@ class HomeController < ApplicationController
   	to = Date.civil(@current_year,12,31)
 
   	@country = request.location.country
-  	@country_code = request.location.country_code.downcase
+  	country_code = request.location.country_code.downcase
   	
-  	upcoming_holidays = Holidays.between(from, to, @country_code.to_sym)
-  	@next_holiday_name = upcoming_holidays[0][:name]
-  	@next_holiday_date = upcoming_holidays[0][:date]
-  	@next_holiday_month_day = @next_holiday_date.day
-  	@next_holiday_month = @next_holiday_date.strftime('%B')
-  	@next_holiday_week_day = @next_holiday_date.strftime('%A')
+  	@upcoming_holidays = Holidays.between(from, to, country_code.to_sym)
+  	
+  	unless @upcoming_holidays.nil?
+	  	next_holiday_date = @upcoming_holidays[0][:date]
+	  	@next_holiday_name = @upcoming_holidays[0][:name]
+	  	@next_holiday_week_day = next_holiday_date.strftime('%A')
+	  	@next_holiday_month = next_holiday_date.strftime('%B')
+	  	@next_holiday_month_day = next_holiday_date.day 	
+	  end
   end
 end
